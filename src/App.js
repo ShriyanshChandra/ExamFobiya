4998880f4d0ddcbfd4c33d851e2d6d8f565e46b5
Shriyansh Chandra <chandrashriyansh@gmail.com>
2025-11-26 16:39:10 +0530

    dark mode implementation and fix

--- src/App.js
+++ src/App.js
@@ -6,27 +6,30 @@ import Books from "./pages/Books";
 import Distributors from "./pages/Distributors";
 import Papers from "./pages/Papers";
 import AboutUs from "./pages/About_us";
-import "@fontsource/nunito"; 
+import { ThemeProvider } from "./context/ThemeContext";
+import "@fontsource/nunito";
 
-import './App.css'; 
+import './App.css';
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-      <Navbar setSearchQuery={setSearchQuery} />
-      {/* Ensure this div is transparent so the background shows through */}
-      <div className="p-6">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/books" element={<Books searchQuery={searchQuery} />} />
-          <Route path="/distributors" element={<Distributors />} />
-          <Route path="/papers" element={<Papers />} />
-          <Route path="/about" element={<AboutUs />} />
-        </Routes>
-      </div>
-    </Router>
+    <ThemeProvider>
+      <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <Navbar setSearchQuery={setSearchQuery} />
+        {/* Ensure this div is transparent so the background shows through */}
+        <div className="p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/books" element={<Books searchQuery={searchQuery} />} />
+            <Route path="/distributors" element={<Distributors />} />
+            <Route path="/papers" element={<Papers />} />
+            <Route path="/about" element={<AboutUs />} />
+          </Routes>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
