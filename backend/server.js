f616a4c191a1bd5188a3de7185f8d1cfdb79124b
Shriyansh Chandra <chandrashriyansh@gmail.com>
2025-12-31 12:58:36 +0530

    fix: force email transporter to use IPv4

--- backend/server.js
+++ backend/server.js
@@ -31,6 +31,7 @@ app.post('/send-otp', async (req, res) => {
             tls: {
                 rejectUnauthorized: false
             },
+            family: 4, // Force IPv4
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS
